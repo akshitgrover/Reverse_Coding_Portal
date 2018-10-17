694d63e58664fcd985cbc654be477721220ef163
Akshit Grover <akshit.grover2016@gmail.com>
2018-10-18 00:08:58 +0530

    Add cors middleware

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ const QuestionRouter = require("./controllers/question.js");
 const AttemptRouter = require("./controllers/attempts.js");
 const TeamRouter = require("./controllers/team.js");
 
+app.use((req, res)=>{
+    res.set("Access-Control-Allow-Origin", "*");
+});
+
 app.use("/admin", AdminRouter);
 app.use("/question", QuestionRouter);
 app.use("/attempt", AttemptRouter);
